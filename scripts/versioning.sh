11e723b134424c7e0a2bc95ebd38699fa27583e5
ゆきかせ <casvstb@gmail.com>
2025-05-10 20:32:03 +0200

    chore: pipeline debug

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -7,8 +7,5 @@ if [ "$CODE" -eq 0 ]; then
   echo "release_needed=true" >> $GITHUB_OUTPUT
   echo "version=$VERSION" >> $GITHUB_OUTPUT
 else
-  echo "release_needed=false" >> $GITHUB_OUTPUT
+  exit 1
 fi
-
-echo $GITHUB_OUTPUT
-echo $VERSION
