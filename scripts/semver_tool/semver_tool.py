752f6bfadd9b92f88e5199dfbfc43dba559b5cd8
ゆきかせ <casvstb@gmail.com>
2025-05-23 21:15:25 +0200

    chore(semver-tool): add version override function

--- scripts/semver_tool/semver_tool.py
+++ scripts/semver_tool/semver_tool.py
@@ -9,15 +9,21 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("project")
     parser.add_argument("-e", "--env", default="main", choices=version_utils.envs.keys())
+    parser.add_argument("-o", "--override")
     args = parser.parse_args()
 
     project = args.project
     env = args.env
+    version_override = args.override
 
     current_version = version_utils.get_current_version(project, env)
     main_version = version_utils.get_current_version(project, "main")
     commits = git_utils.get_commits(project)
-    new_version = version_utils.get_version(current_version, main_version, commits, project)
+
+    if version_override is None:
+        new_version = version_utils.get_version(current_version, main_version, commits, project)
+    else:
+        new_version = version_utils.Version.from_string(version_override)
 
     if new_version == current_version or new_version is None:
         exit(1)
